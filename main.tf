5cdf8c6fd3661098f92e01b7af9c802c9dd0e30c
visithag <visithagalhena@gmail.com>
2025-04-18 11:36:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource group"
+  type        = map(string)
+  default     = {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
