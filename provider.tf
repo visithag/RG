9deb9c3815fdc584ad955910e826f9eb3b12e63e
visithag <visithagalhena@gmail.com>
2025-04-18 12:48:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
 provider "azurerm" {
   features {}
 }
-# Required for Azure Provider v4.0+
-  subscription_id = "xxxxx"  # Replace with actual ID
-  client_id       = "xxxxx"        # Service Principal App ID
-  client_secret   = "xxxxx"    # Service Principal Secret
-  tenant_id       = "xxxxx"        # Azure AD Tenant ID
-}
