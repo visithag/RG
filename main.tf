d15bcb2b975642c8417903b76c6e93acfaf63d8a
visithag <visithagalhena@gmail.com>
2025-04-18 14:01:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,10 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-  backend "azurerm" {}
+# Generate a unique name for the resource group
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-provider "azurerm" {
-  features {}
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure region where the resource group will be created"
-  type        = string
-}
-
-variable "tags" {
-  description = "A map of tags to assign to the resource group"
-  type        = map(string)
-  default     = {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-  tags     = var.tags
+# Create the resource group
+resource "azurerm_resource_group" "example" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
 }
